f09c4045aa2deb0ec66627ae8de26276a0bb009b
David García Ramallal <91455051+david-ramallal@users.noreply.github.com>
2021-11-25 12:04:20 +0100

    Entorno aux functions added

--- p3/shell.c
+++ p3/shell.c
@@ -30,6 +30,7 @@
 #include <ctype.h>
 
 #define MAXLINEA 1024
+#define MAXVAR 1024
 #define LEERCOMPLETO ((ssize_t)-1)
 
 TCMDLIST L;
@@ -1309,6 +1310,47 @@ void cmd_es(char *tr[]){
 			WriteFichero(overWrite, fich, init, count);
 		}		
 	}
+}
+
+void MostrarEntorno (char **entorno, char * nombre_entorno)
+{
+	int i=0;
+	while (entorno[i]!=NULL) {
+		printf ("%p->%s[%d]=(%p) %s\n", &entorno[i],
+		nombre_entorno, i,entorno[i],entorno[i]);
+		i++;
+	}
+}
+
+int BuscarVariable (char * var, char *e[])
+{
+	int pos=0;
+	char aux[MAXVAR];
+	strcpy (aux,var);
+	strcat (aux,"=");
+	while (e[pos]!=NULL)
+		if (!strncmp(e[pos],aux,strlen(aux)))
+			return (pos);
+		else
+			pos++;
+	errno=ENOENT;
+	/*no hay tal variable*/
+	return(-1);
+}
+
+int CambiarVariable(char * var, char * valor, char *e[])
+{
+	int pos;
+	char *aux;
+	if ((pos=BuscarVariable(var,e))==-1)
+		return(-1);
+	if ((aux=(char *)malloc(strlen(var)+strlen(valor)+2))==NULL)
+		return -1;
+	strcpy(aux,var);
+	strcat(aux,"=");
+	strcat(aux,valor);
+	e[pos]=aux;
+	return (pos);
 }
 
 void cmd_priority(char *tr[]){
